6ae9b8c6ae4e3f1b279fd51edf8f086a8c77b4c6
Stan <stan_gai@Hotmail.com>
2017-05-21 15:21:45 +1000

    ArrayList部分功能实现

---
+++ src/com/github/user3301/datastructures/List/MyArrayList.java
@@ -0,0 +1,108 @@
+package com.github.user3301.datastructures.List;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+/**
+ * Created by Zhelun Gai on 21/5/17.
+ */
+public class MyArrayList<T> implements Iterable<T>{
+    private static final int DEFAULT_CAPCITY = 10;
+    private int size;
+    private T[] data;
+
+    /**
+     * constructor 1
+     */
+    public MyArrayList() {
+        this.data = (T[]) new Object[DEFAULT_CAPCITY];
+    }
+
+    /**
+     * constructor 2
+     * @param capacity
+     */
+    public MyArrayList(int capacity) {
+        this.data = (T[]) new Object[capacity];
+    }
+
+    /**
+     * constructor 3
+     * @param collection
+     */
+    public MyArrayList(Collection<T> collection) {
+        this.data = (T[]) new Object[collection.toArray().length];
+        this.size = data.length;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return getSize()==0;
+    }
+
+    public T get(int index) {
+        if (index<0||index>=getSize()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return this.data[index];
+    }
+
+    public void add(T element) {
+        if(getSize()+1>=this.data.length) {
+            ensureCapacity(size*2+1);
+        }
+        this.data[size++] = element;
+    }
+
+    private void ensureCapacity(int newCapacity) {
+        if(newCapacity<this.size)
+            return;
+        T[] old = data;
+        data = (T[]) new Object[newCapacity];
+        for (int i = 0; i < this.data.length; i++) {
+            data[i] = old[i];
+        }
+    }
+
+    public void remove(T element) {
+        int index = indexOf(element);
+        for (int i = index; i < getSize(); i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+    }
+
+    private int indexOf(T element) {
+        int index = 0;
+        for (int i = 0; i <getSize(); i++) {
+            if(data[i]==element) {
+                index = i;
+            }
+        }
+//        if (index==-1) {
+//            throw new IndexOutOfBoundsException();
+//        }
+        return index;
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return null;
+    }
+}
---
+++ src/com/github/user3301/datastructures/Test/MyArrayListTest.java
@@ -0,0 +1,45 @@
+package com.github.user3301.datastructures.Test;
+
+import com.github.user3301.datastructures.List.MyArrayList;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/** MyArrayList Junit test
+ * Created by Stan on 21/5/17.
+ */
+public class MyArrayListTest {
+
+    @Test
+    public void isEmptyTest() {
+        MyArrayList myArrayList = new MyArrayList();
+        assertEquals(true,myArrayList.isEmpty());
+        myArrayList.add(new Integer(1));
+        assertEquals(false,myArrayList.isEmpty());
+    }
+
+    @Test
+    public void getSizeTest() {
+        MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add(new Integer(1));
+        myArrayList.add(new Integer(2));
+        myArrayList.add(new Integer(3));
+        assertEquals(3,myArrayList.getSize());
+    }
+
+    @Test
+    public void removeTest() {
+        MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add(new Integer(1));
+        myArrayList.remove(1);
+        assertEquals(true,myArrayList.isEmpty());
+    }
+
+    @Test
+    public void getTest() {
+        MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add(new Integer(1));
+        assertEquals(1,myArrayList.get(0));
+    }
+}
